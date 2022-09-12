3c54d2aaa6aa713a816d2bc6af7c56bdd8b44513
Hassan Hosseini <hassankhosseini@gmail.com>
2022-09-12 11:49:38 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,7 +18,7 @@ resource "aws_instance" "web_app" {
     device_name = "my_data"
     volume_type = "io1"
     volume_size = 300
-    iops        = 100                      # <<<<< Try changing this to 10000 to compare costs
+    iops        = 150                      # <<<<< Try changing this to 10000 to compare costs
   }
 }
 
